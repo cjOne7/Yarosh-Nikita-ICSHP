4ff855bbe4d33fd9fc27266d1c97b5726818a6a3
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-03-17 18:26:33 +0100

    Update Form1.Designer.cs

--- Cv4/Cv4/Form1.Designer.cs
+++ Cv4/Cv4/Form1.Designer.cs
@@ -24,7 +24,7 @@
       /// the contents of this method with the code editor.
       /// </summary>
       private void InitializeComponent() {
-         this.listBox1 = new System.Windows.Forms.ListBox();
+         this.gameListBox = new System.Windows.Forms.ListBox();
          this.statusStrip1 = new System.Windows.Forms.StatusStrip();
          this.correctLabel = new System.Windows.Forms.ToolStripStatusLabel();
          this.missedLabel = new System.Windows.Forms.ToolStripStatusLabel();
@@ -32,26 +32,30 @@
          this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
          this.difficultProgressBar = new System.Windows.Forms.ToolStripProgressBar();
          this.timer1 = new System.Timers.Timer();
+         this.StartNewGameBtn = new System.Windows.Forms.Button();
+         this.ExitBtn = new System.Windows.Forms.Button();
+         this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
          this.statusStrip1.SuspendLayout();
          ((System.ComponentModel.ISupportInitialize) (this.timer1)).BeginInit();
+         this.flowLayoutPanel1.SuspendLayout();
          this.SuspendLayout();
          // 
-         // listBox1
+         // gameListBox
          // 
-         this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
-         this.listBox1.FormattingEnabled = true;
-         this.listBox1.ItemHeight = 51;
-         this.listBox1.Items.AddRange(new object[] {"q", "q", "q"});
-         this.listBox1.Location = new System.Drawing.Point(27, 76);
-         this.listBox1.MultiColumn = true;
-         this.listBox1.Name = "listBox1";
-         this.listBox1.Size = new System.Drawing.Size(724, 55);
-         this.listBox1.TabIndex = 0;
+         this.gameListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+         this.gameListBox.FormattingEnabled = true;
+         this.gameListBox.ItemHeight = 135;
+         this.gameListBox.Items.AddRange(new object[] {"q", "q", "q"});
+         this.gameListBox.Location = new System.Drawing.Point(0, 49);
+         this.gameListBox.MultiColumn = true;
+         this.gameListBox.Name = "gameListBox";
+         this.gameListBox.Size = new System.Drawing.Size(800, 139);
+         this.gameListBox.TabIndex = 0;
          // 
          // statusStrip1
          // 
          this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.correctLabel, this.missedLabel, this.accurancyLabel, this.toolStripStatusLabel4, this.difficultProgressBar});
-         this.statusStrip1.Location = new System.Drawing.Point(0, 227);
+         this.statusStrip1.Location = new System.Drawing.Point(0, 188);
          this.statusStrip1.Name = "statusStrip1";
          this.statusStrip1.Size = new System.Drawing.Size(800, 25);
          this.statusStrip1.TabIndex = 1;
@@ -95,13 +99,44 @@
          this.timer1.Interval = 800D;
          this.timer1.SynchronizingObject = this;
          // 
+         // StartNewGameBtn
+         // 
+         this.StartNewGameBtn.Location = new System.Drawing.Point(3, 3);
+         this.StartNewGameBtn.Name = "StartNewGameBtn";
+         this.StartNewGameBtn.Size = new System.Drawing.Size(90, 37);
+         this.StartNewGameBtn.TabIndex = 2;
+         this.StartNewGameBtn.Text = "New game";
+         this.StartNewGameBtn.UseVisualStyleBackColor = true;
+         this.StartNewGameBtn.Click += new System.EventHandler(this.button1_Click);
+         // 
+         // ExitBtn
+         // 
+         this.ExitBtn.Location = new System.Drawing.Point(99, 3);
+         this.ExitBtn.Name = "ExitBtn";
+         this.ExitBtn.Size = new System.Drawing.Size(64, 36);
+         this.ExitBtn.TabIndex = 3;
+         this.ExitBtn.Text = "Exit";
+         this.ExitBtn.UseVisualStyleBackColor = true;
+         this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+         // 
+         // flowLayoutPanel1
+         // 
+         this.flowLayoutPanel1.Controls.Add(this.StartNewGameBtn);
+         this.flowLayoutPanel1.Controls.Add(this.ExitBtn);
+         this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+         this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+         this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+         this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 43);
+         this.flowLayoutPanel1.TabIndex = 5;
+         // 
          // Form1
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(800, 252);
+         this.ClientSize = new System.Drawing.Size(800, 213);
+         this.Controls.Add(this.flowLayoutPanel1);
          this.Controls.Add(this.statusStrip1);
-         this.Controls.Add(this.listBox1);
+         this.Controls.Add(this.gameListBox);
          this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
          this.MaximizeBox = false;
          this.MinimizeBox = false;
@@ -110,9 +145,17 @@
          this.statusStrip1.ResumeLayout(false);
          this.statusStrip1.PerformLayout();
          ((System.ComponentModel.ISupportInitialize) (this.timer1)).EndInit();
+         this.flowLayoutPanel1.ResumeLayout(false);
          this.ResumeLayout(false);
          this.PerformLayout();
       }
+
+      private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+
+      private System.Windows.Forms.Button ExitBtn;
+      private System.Windows.Forms.Button StartNewGameBtn;
+
+      private System.Windows.Forms.ListBox gameListBox;
 
       private System.Timers.Timer timer1;
 
@@ -127,8 +170,6 @@
       private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
 
       private System.Windows.Forms.StatusStrip statusStrip1;
-
-      private System.Windows.Forms.ListBox listBox1;
 
       #endregion
    }
