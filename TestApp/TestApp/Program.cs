ca28a2a824bd7eca7508ef580dc90fe3b5fbd30f
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-01 00:14:07 +0200

    Update Program.cs

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace TestApp {
    internal class Program {
       public static void Main(string[] args) {
-         var linkedList = new LinkedList();
-         linkedList.Add(1);
-         linkedList.Add(2);
-         linkedList.Add(3);
-         linkedList.Insert(1, 1000);
-         linkedList[linkedList.Count - 1] = 200;
+         Gamers gamers = new Gamers();
+         gamers.Add(new Gamer("Ronaldo", FootballClub.RealMadrid, 10));
+         gamers.Add(new Gamer("Messi", FootballClub.Barcelona, 10));
+         gamers.Add(new Gamer("F1", FootballClub.Chelsea, 8));
+         gamers.Add(new Gamer("F2", FootballClub.Arsenal, 3));
          
-         linkedList.Remove(1);
-         for (var i = 0; i < linkedList.Count; i++){
-            Console.WriteLine(linkedList[i]);
-         }
          
-         var list = new LinkedList<object>();
-         
+         gamers.FindTheBestClub(out var clubs, out var goals);
+         Console.WriteLine(goals);
+         Array.ForEach(clubs, club => Console.WriteLine(club));
+
+
+         // var linkedList = new LinkedList();
+         // linkedList.Add(1);
+         // linkedList.Add(2);
+         // linkedList.Add(3);
+         // linkedList.Insert(1, 1000);
+         // linkedList[linkedList.Count - 1] = 200;
+         // // linkedList.Remove(1);
+         // var enumerator = linkedList.GetEnumerator();
+         // while (enumerator.MoveNext()){
+         //    object o = enumerator.Current;
+         //    Console.WriteLine(o);
+         // }
+         // // for (var i = 0; i < linkedList.Count; i++){
+         // //    Console.WriteLine(linkedList[i]);
+         // // }
       }
    }
 }
