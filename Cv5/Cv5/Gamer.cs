f18d6f24d4b71ee1c0571ea281d8bd80d66633e2
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-01 01:48:20 +0200

    Update Gamer.cs

--- Cv5/Cv5/Gamer.cs
+++ Cv5/Cv5/Gamer.cs
@@ -2,19 +2,17 @@
    public class Gamer {
       public string Name { get; set; }
 
-      public FootballClub Club { get; set;}
+      public FootballClub Club { get; set; }
 
-      public int GoalsNumber { get; set;}
-      
+      public int GoalsNumber { get; set; }
+
       public Gamer(string name, FootballClub club, int goalsNumber) {
          Name = name;
          Club = club;
          GoalsNumber = goalsNumber;
       }
 
-      public Gamer() {
-         
-      }
+      public Gamer() { }
 
       public override string ToString() {
          return $"Name: {Name}, club: {Club}, goals: {GoalsNumber}";
