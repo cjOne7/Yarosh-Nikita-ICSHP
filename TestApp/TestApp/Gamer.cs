1a43ae3152636f72a2da6fed22c1030c14ff8d8d
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-01 00:14:05 +0200

    Update Gamer.cs

--- TestApp/TestApp/Gamer.cs
+++ TestApp/TestApp/Gamer.cs
@@ -1,15 +1,19 @@
 ﻿namespace TestApp {
    public class Gamer {
-      public string Name { get; }
+      public string Name { get; set; }
 
-      public FootballClub Club { get; }
+      public FootballClub Club { get; set;}
 
-      public int GoalsNumber { get; }
+      public int GoalsNumber { get; set;}
       
       public Gamer(string name, FootballClub club, int goalsNumber) {
          Name = name;
          Club = club;
          GoalsNumber = goalsNumber;
+      }
+
+      public Gamer() {
+         
       }
 
       public override string ToString() {
