7437b26cbfd93ca4ecd157097873ad3301f03ec0
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-02 20:11:42 +0200

    Create OptionsForm.cs

---
+++ Ping_Pong_Yarosh_v1/Ping_Pong_Yarosh_v1/OptionsForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Ping_Pong_Yarosh_v1 {
+   public partial class OptionsForm : Form {
+      public OptionsForm() {
+         InitializeComponent();
+      }
+
+      private void ConfirmBtn_Click(object sender, EventArgs e) {
+         
+      }
+
+      private void CancelBtn_Click(object sender, EventArgs e) {
+         Close();
+      }
+
+      private void MouseRadioBtn_CheckedChanged(object sender, EventArgs e) {
+         MovingLeftTextBox.Enabled = false;
+         MovingRightTextBox.Enabled = false;
+      }
+
+      private void KeyboardRadioBtn_CheckedChanged(object sender, EventArgs e) {
+         MovingLeftTextBox.Enabled = true;
+         MovingRightTextBox.Enabled = true;
+         
+      }
+   }
+}
