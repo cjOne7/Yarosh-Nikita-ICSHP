291327c4d06eaf2cf08e6deaf9caf556e91fbeb1
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-02 20:11:50 +0200

    Create KeyboardControl.cs

---
+++ Ping_Pong_Yarosh_v1/Ping_Pong_Yarosh_v1/KeyboardControl.cs
@@ -0,0 +1,15 @@
+﻿﻿namespace TestApp {
+   public class KeyboardControl {
+      public string Left { get; }
+      public string Right { get; }
+
+      public KeyboardControl(string left, string right) {
+         Left = left;
+         Right = right;
+      }
+
+      public override string ToString() {
+         return $"Left: {Left}, Right: {Right}";
+      }
+   }
+}
