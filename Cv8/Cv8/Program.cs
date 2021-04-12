230ff3ccf839f5937fd843665c6d523e96b91c82
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2021-04-12 17:30:42 +0200

    Update Program.cs

--- Cv8/Cv8/Program.cs
+++ Cv8/Cv8/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Cv8 {
+﻿using System;
+
+namespace Cv8 {
    internal class Program {
       public static void Main(string[] args) {
-         
+         var table = new MinMaxHashTable<int, int>();
+         table.Add(10, 10);
+         table.Add(20, 20);
+         table.Add(30, 30);
+         table.Add(40, 40);
+         table.Add(50, 50);
+         table.Add(70, 70);
+         Console.WriteLine(table.Contains(40));
       }
    }
 }
